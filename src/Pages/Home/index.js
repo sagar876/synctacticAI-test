4d828be41e87a10bd3337984f768f8493dbe4aaa
Sagar S. Dixit <sagar.dixit@SagarDixit-Mac.local>
2019-11-01 19:45:05 +0530

    initial project setup with pages,components

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Header from "../../Components/Header/header";
+import MapComponent from "../../Components/Map/map";
+import NavBar from "../../Components/NavigationBar/navbar";
+import TextBox from "../../Components/CustomComponents/textbox";
+
+
+export default class HomePage extends Component {
+  
+  render() {
+    const { navbarOpen } = this.state;
+    return (
+      <div className="home--wrapper">
+       
+      </div>
+    );
+  }
+}
